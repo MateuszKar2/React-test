7f15b75cdfda5ec296b4a6f4b012bab43af9a0fd
MateuszKar2 <system505@protonmail.com>
2023-09-09 21:10:42 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,22 @@
+import {Counter} from './Counter/Counter'
 
-
-import { Product } from './Product';
-<Product 
-price= '20'
-name= "Jordan"
-buttonText='Okej'
-/>
-
-const favoriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-  { id: "id-4", name: "Redux in depth" },
-];
-
-const BookList = ({ books }) => {
+export const App = () => {
   return (
-    <ul>
-      {
-        books.map(({id, name}, index) => (
-          <li key={id}>
-            {index + 1}{" "}{name}
-          </li>
-        ))
-      }
-    </ul>
-  )
-}
-
-const Profile = ({ name, email, children}) => (
-  <div>
-    <p>Name: {name}</p>
-    <p>Email: {email}</p>
-    <ul>
-    <li>{children}</li>
-    </ul>
-  </div>
-)
-
-const Panel = ({ title, children }) => (
-  <section>
-    <h2>{title}</h2>
-    {children}
-  </section>
-)
-
-const Mailbox = ({ username, unreadMessages }) => (
-    <div>
-      <h1>Hello {username}!</h1>
-      <p>
-      {
-        unreadMessages.length > 0 ? 
-          `You have ${unreadMessages.length} unread messages.`
-           : "No unread messages"
-      }
-      </p>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <>
+        <Counter/>
+      </>
+      React homework template ok
     </div>
-  )
-
-
-function App() {
-  return (
-    <div>
-      <BookList books={favoriteBooks}/>
-    </div>
+    
   );
-}
-
-export default App;
+};
