04dc6b7b9b04ba83595807d9150fbfb28fb9715d
MateuszKar2 <system505@protonmail.com>
2023-09-10 18:03:36 +0200

    add file

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,13 @@
+export const BookList = ({ books }) => {
+    return (
+      <ul>
+        {
+          books.map(({id, name}, index) => (
+            <li key={id}>
+              {index + 1}{" "}{name}
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
