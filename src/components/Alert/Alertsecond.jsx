d9022a7d05ea637fca0287a0f93d9e59e590b0c9
MateuszKar2 <system505@protonmail.com>
2023-09-13 18:48:54 +0200

    add file

--- src/components/Alert/Alertsecond.jsx
+++ src/components/Alert/Alertsecond.jsx
@@ -1,16 +1,31 @@
 //Vanilia CSS
-
+import clsx from "clsx";
 import "./Alert.css"
 
+// const Alertsecond = ({ variant, outlined, elevated, children }) => {
+//     const className = ["alert", variant];
+
+//     if (outlined) className.push("is-outlined")
+//     if (elevated) className.push("is-elevated")
+
+//     return (
+//         <p className={className.join(" ")}>{children}</p>
+//     )
+// };
+
 const Alertsecond = ({ variant, outlined, elevated, children }) => {
-    const className = ["alert", variant];
-
-    if (outlined) className.push("is-outlined")
-    if (elevated) className.push("is-elevated")
-
     return (
-        <p className={className.join(" ")}>{children}</p>
+        <p
+            className={clsx(
+                "alert",
+                variant,
+                outlined && "is-outlined",
+                elevated && "is-elevated",
+            )}
+         >
+            {children}
+        </p>
     )
-};
+}
 
 export default Alertsecond;
