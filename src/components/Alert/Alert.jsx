19fa4646047b62b1a397d85091351d8fd4b39d2e
MateuszKar2 <system505@protonmail.com>
2023-09-13 11:23:11 +0200

    add file

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,9 +1,34 @@
+//STYLIZACJA - style wbudowane
+ const alertStyle = {
+    color: "red",
+    margin: 100,
+    padding: 20,
+    backgroundColor: "yellow",
+    textAlign: "center",
+    fontSize: 35,
+    fontWeight: 700,
+};
 
-import clsx from "clsx"
-export const Alert = (name) => {
+const getBgColor = variant => {
+    switch (variant) {
+        case "info":
+            return "blue";
+        case "success":
+            return "green";
+        case "error":
+            return "orange";
+        default:
+            throw new Error(`Unsupported variant prop value - ${variant}`);
+    }
+}
+
+export const Alert = ({ children, variant }) => {
     return (
-        <>{name}</>
+        <p
+        style={{...alertStyle, backgroundColor: getBgColor(variant) }}>
+            {children}
+        </p>
     )
 }
 
-export default Alert;
+export default alertStyle;
