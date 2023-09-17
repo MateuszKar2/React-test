528da90a2497b74bcea96208a0c79ce4b5934379
MateuszKar2 <system505@protonmail.com>
2023-09-17 16:37:53 +0200

    add file

--- src/components/Events/Eventsmain.jsx
+++ src/components/Events/Eventsmain.jsx
@@ -1,9 +1,13 @@
 import Events from "./Events";
+import Toggle from "./Toggle";
+import Message from "./Message";
 
 export const Eventsmain = () => {
     return (
         <>
             <Events/>
+            <Toggle/>
+            <Message/>
         </>
     )
 }
