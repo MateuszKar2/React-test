37ca3e89a83735fce9b5cc5b724266ffdad8c6fa
MateuszKar2 <system505@protonmail.com>
2023-09-17 16:37:45 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,47 @@
 
-import { BookList } from "./BookList/BookList";
-import { Product } from "./Product/Product";
-import cheese from "./pictures/cheese.png"
+// import { BookList } from "./BookList/BookList";
+// import { Product } from "./Product/Product";
+// import cheese from "./pictures/cheese.png"
 
-import Alertmain from "./Alert/Alertmain";
+// import Alertmain from "./Alert/Alertmain";
 
 import Eventsmain from "./Events/Eventsmain";
 
 
 
-const favoriteBooks = [
- { id: "id-1", name: "JS for beginners" },
- { id: "id-2", name: "React basics" },
- { id: "id-3", name: "React Router overview" },
- { id: "id-4", name: "Redux in depth" },
-];
+// const favoriteBooks = [
+//  { id: "id-1", name: "JS for beginners" },
+//  { id: "id-2", name: "React basics" },
+//  { id: "id-3", name: "React Router overview" },
+//  { id: "id-4", name: "Redux in depth" },
+// ];
 
-const Profile = ({ name, email }) => (
- <div>
-   <p>name: {name}</p>
-   <p>email: {email}</p>
- </div>
-)
+// const Profile = ({ name, email }) => (
+//  <div>
+//    <p>name: {name}</p>
+//    <p>email: {email}</p>
+//  </div>
+// )
 
-const Panel = ({ title, children}) => (
- <section>
-    <h2>{title}</h2>
-    {children}
- </section>
-)
+// const Panel = ({ title, children}) => (
+//  <section>
+//     <h2>{title}</h2>
+//     {children}
+//  </section>
+// )
 
-const Mailbox = ({ unreadMessage, username }) => {
- return (
-   <div>
-     <h1>Hello {username}!</h1>
-     {
-       unreadMessage.length > 0 ? 
-           `You have ${unreadMessage.length} unread message.`
-           : "No unread message"
-     }
-   </div>
- )
-}
+// const Mailbox = ({ unreadMessage, username }) => {
+//  return (
+//    <div>
+//      <h1>Hello {username}!</h1>
+//      {
+//        unreadMessage.length > 0 ? 
+//            `You have ${unreadMessage.length} unread message.`
+//            : "No unread message"
+//      }
+//    </div>
+//  )
+// }
 
 
 
