589b87bd6cc06246964ecda13eeb7796bad9baf1
MateuszKar2 <system505@protonmail.com>
2023-09-18 11:55:13 +0200

    add file

--- src/components/Events/Eventsmain.jsx
+++ src/components/Events/Eventsmain.jsx
@@ -2,14 +2,25 @@ import Events from "./Events";
 import Toggle from "./Toggle";
 import Message from "./Message";
 import Form from "./Form";
+import LittleFrom from "./LittleForm";
+import HardForm from "./HardForm";
 
 export const Eventsmain = () => {
     return (
         <>
+        <div
+        style={{
+            margin: 20,
+            padding: 20,
+        }}
+        >
             <Events/>
             <Toggle/>
             <Message/>
             <Form/>
+            <LittleFrom/>
+            <HardForm/>
+        </div>
         </>
     )
 }
