826f25be3dcbe076f1d3b1acb138d3a0ccafe6c5
MateuszKar2 <system505@protonmail.com>
2023-09-25 14:09:07 +0200

    add file

---
+++ src/components/LiveCycle/LifeCycleMain.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import LifeCycle from "./LifeCycle"
+
+class LifeCycleMain extends Component {
+
+    render() {
+        return(
+            <>
+                <LifeCycle>
+
+                </LifeCycle>
+            </>
+        )
+    }
+}
+
+export default LifeCycleMain;
