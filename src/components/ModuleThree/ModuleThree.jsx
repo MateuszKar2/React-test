1130bab16df6be7275a7bb75d5ebb91e6e8a6cfd
MateuszKar2 <system505@protonmail.com>
2023-09-25 15:51:06 +0200

    add file

---
+++ src/components/ModuleThree/ModuleThree.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import LifeCycle from "./LifeCycle"
+import HTTPrequests from "./HTTPrequests";
+
+class ModuleThree extends Component {
+
+    render() {
+        return(
+            <>
+                <LifeCycle>
+
+                </LifeCycle>
+
+                <HTTPrequests>
+                    
+                </HTTPrequests>
+            </>
+        )
+    }
+}
+
+export default ModuleThree;
