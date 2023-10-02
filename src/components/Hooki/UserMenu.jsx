4b7035b2a3c4de717d8c0733a930543ae4f5eda0
MateuszKar2 <system505@protonmail.com>
2023-10-02 22:06:19 +0200

    add file

--- src/components/Hooki/UserMenu.jsx
+++ src/components/Hooki/UserMenu.jsx
@@ -1,16 +1,16 @@
 import { useUser } from "./userContext";
 
-export const  UserMenu = () => {
-    const { isLoggedIn, username, logIn, logOut } = UseUser();
+export const UserMenu = () => {
+  const { isLoggedIn, username, logIn, logOut } = useUser();
 
-    return (
-        <div>
-          {isLoggedIn && <p>{username}</p>}
-          {isLoggedIn ? (
-            <button onClick={logOut}>Log out</button>
-          ) : (
-            <button onClick={logIn}>Log in</button>
-          )}
-        </div>
-      );
-}
+  return (
+    <div>
+      {isLoggedIn && <p>{username}</p>}
+      {isLoggedIn ? (
+        <button onClick={logOut}>Log out</button>
+      ) : (
+        <button onClick={logIn}>Log in</button>
+      )}
+    </div>
+  );
+};
