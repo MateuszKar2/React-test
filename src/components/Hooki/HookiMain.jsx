4d5f1e277798b81e9a11417eedd890f22a546435
MateuszKar2 <system505@protonmail.com>
2023-10-02 18:48:23 +0200

    add file

--- src/components/Hooki/HookiMain.jsx
+++ src/components/Hooki/HookiMain.jsx
@@ -1,16 +1,25 @@
 import { Book } from "./UseState"
 import { Price } from "./UseEfect";
+import { SeveralEffects } from "./SeveralEffects";
+import { UserMenu } from "./UserMenu";
+
+
+
 
 
 
 export const HookiMain = () => {
 
+
     return (
         <>
             <Book/>
+            <SeveralEffects/>
             <Price/>
+                <UserProvider>
+                    <UserMenu />
+                </UserProvider>
         </>
     )
 }
 
-export default HookiMain;
