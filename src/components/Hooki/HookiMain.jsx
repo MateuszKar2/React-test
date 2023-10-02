288a239d8cf66fb674628ab3ab549a3364299325
MateuszKar2 <system505@protonmail.com>
2023-10-02 14:48:30 +0200

    add file

---
+++ src/components/Hooki/HookiMain.jsx
@@ -0,0 +1,16 @@
+import { Book } from "./UseState"
+import { Price } from "./UseEfect";
+
+
+
+export const HookiMain = () => {
+
+    return (
+        <>
+            <Book/>
+            <Price/>
+        </>
+    )
+}
+
+export default HookiMain;
