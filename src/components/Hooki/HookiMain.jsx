9db0d6fec52ed1c2376239b2d283e4b8bd32362a
MateuszKar2 <system505@protonmail.com>
2023-10-03 19:19:04 +0200

    add Memo

--- src/components/Hooki/HookiMain.jsx
+++ src/components/Hooki/HookiMain.jsx
@@ -3,6 +3,8 @@ import { Price } from "./UseEfect";
 import { SeveralEffects } from "./SeveralEffects";
 import { MyUseRef } from "./MyUseRef";
 import { Player } from "./Player";
+import { Planets } from "./UseMemo";
+import { UseMemo } from "./UseMemo";
 // import { UserMenu } from "./UserMenu";
 
 
@@ -23,6 +25,8 @@ export const HookiMain = () => {
                 </UserProvider> */}
             <MyUseRef/>
             <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
+            <Planets/>
+            <UseMemo></UseMemo>
         </>
     )
 }
