f07ea3e7edc6a782b38a5d42e984b3544adbb41e
MateuszKar2 <system505@protonmail.com>
2023-10-10 11:20:32 +0200

    add file

---
+++ src/components/09-nawigacja/Reviews.jsx
@@ -0,0 +1,10 @@
+
+
+export const Reviews = () => {
+    return( 
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Fuga aspernatur quibusdam saepe sapiente dignissimos quaerat?</p>
+        </>
+    )
+}
