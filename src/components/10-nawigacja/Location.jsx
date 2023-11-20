20e69a2b2634cfad37a6431ba7f433a681237ee0
MateuszKar2 <system505@protonmail.com>
2023-11-20 15:58:03 +0100

    add file

--- src/components/10-nawigacja/Location.jsx
+++ src/components/10-nawigacja/Location.jsx
@@ -1,21 +1,28 @@
+// Każdy zapis w stosie historii nawigacji jest opisany obiektem lokalizacji (location)
+//  ze standardowym zestawem właściwości, które przechowują pełną informację o URL.
+//   Gdy użytkownik klika na odnośnik i przemieszcza się w aplikacji,
+//  bieżąca lokalizacja zmienia się i dodawany jest nowy zapis do historii.
+
+
+
 // Zwraca obiekt lokalizacji przedstawiający bieżący URL za każdym razem, 
 // gdy przechodzimy po nowej ścieżce w Routingu lub aktualizujemy część bieżącego URL. 
 // Jednym z zastosowań może być zadanie, gdzie należy wykonać jakiś efekt przy zmianie bieżącej lokalizacji.
 //  Na przykład wysłanie danych na serwer analityki.
 
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom"
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom"
 
-export const Location = () => {
-    const location = useLocation();
+// export const Location = () => {
+//     const location = useLocation();
 
-    useEffect(() => {
-        Analitycs.send(location);
-    }, [location]);
+//     useEffect(() => {
+//         Analitycs.send(location);
+//     }, [location]);
     
-    return(
-        <>
+//     return(
+//         <>
             
-        </>
-    )
-}
+//         </>
+//     )
+// }
