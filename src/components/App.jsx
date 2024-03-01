a6de7759dd493e31af825a93d359db0675abcec0
MateuszKar2 <system505@protonmail.com>
2024-03-01 20:10:38 +0100

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,11 +42,22 @@
 //  )
 // }
 
-
+import { ReactContent } from "./01-aplikacjeWebowe/pages/ReactContent";
+import { ReactContentTwo } from "./01-aplikacjeWebowe/pages/ReactContentTwo";
+import { Product } from "./01-aplikacjeWebowe/pages/Product";
+import { ProductTwo } from "./01-aplikacjeWebowe/pages/ProductTwo";
 
   export const App = () => {
   return (
     <>
+      <ReactContent></ReactContent>
+      <ReactContentTwo></ReactContentTwo>
+      <Product></Product>
+      <ProductTwo 
+      imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+      price={18.99}
+      name="Burger"
+      />
 
 
    {/* <div
