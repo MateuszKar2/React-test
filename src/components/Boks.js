9030838c8700c574cc2addc5d6538f54d5a35eec
MateuszKar2 <system505@protonmail.com>
2024-05-23 17:55:49 +0200

    add file component

---
+++ src/components/Boks.js
@@ -0,0 +1,40 @@
+
+
+export const Boks = (props) => {
+    <>
+        <p>Title:{props.title}</p>
+            <div>Price:{props.price}</div>
+                <button type="button"></button>
+    </>
+}
+
+export default Boks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Utworzyć komponent
+// przekazać propsy
+// Domyślna wartość propsów
+// props.children
+// propTypes
+// operatory warunkowe, logiczne, ternary
+// zbiory i metoda .map
+
+
+//dodać styl wbudowany(1)
+//Vanilla CSS(1)
+//Napisać CSS-module
+//Dodać do głównego css normalize
