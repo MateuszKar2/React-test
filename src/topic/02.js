1aced5a480a1e1039f2f6abb655603519ee42bd3
MateuszKar2 <system505@protonmail.com>
2024-05-24 16:27:52 +0200

    add notes

--- src/topic/02.js
+++ src/topic/02.js
@@ -1,22 +1,22 @@
-//Stylizacja
-
+`//Stylizacja`
 //Istnieją trzy sposoby dodawania stylów
 
-//Style wbudowane- (najprostsze i najbardziej ograniczone)
+
+`//Style wbudowane- (najprostsze i najbardziej ograniczone)`
 //style używane w komponencie
 //notacja camelCase np.bacgroundColor
 
 
-//VanillaCSS - Boks.css
+`//VanillaCSS - Boks.css`
 //w arkuszu z rozszerzeniem .css stylizacja
 //Problem jest taki... powtarzanie się klas (rozwiązaniem problemu jest biblioteke clsx)
 
 
-//CSS-moduły - Boks.module.css
+`//CSS-moduły - Boks.module.css`
 //wygląda jak zwykły css, ale jest kompilowany do formatu ICSS
 //moduł css posiada domyślny eksport
 //unikalna nazwa [filename]_[classname]__[hash].
 //właściwość composes  (nie trzeba biblioteki clsx) 
 
 
-//Normalize - należy dodać do głównego CSS aby znormalizować style
+`//Normalize - należy dodać do głównego CSS aby znormalizować style`
