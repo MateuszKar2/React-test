da35908ae7386abc992f7eaf325bd7cf847e0614
MateuszKar2 <system505@protonmail.com>
2024-07-26 19:31:42 +0200

    add file

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+import { useUser } from "components/userContext";
+
+const Movies = () => {
+
+
+const { username } = useUser();
+
+const btnRef = useRef();
+
+    return(
+        <>
+            <h2>The best Movies</h2>
+            <p>{username}</p>
+            <button ref={btnRef}>Button with ref</button>
+        </>
+    )
+}
+
+export default Movies;
