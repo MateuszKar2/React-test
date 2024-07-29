b79b3e4f296851be342b5ac6930d57c2a8326d9b
MateuszKar2 <system505@protonmail.com>
2024-07-29 11:06:35 +0200

    add file

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,27 @@ import Spring from "./pages/Spring";
 import Summer from "./pages/Summer";
 import Winter from "./pages/Winter";
 import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+const StyledLink = styled(NavLink)`
+  color: blue;
 
+  &.active {
+    color: red;
+  }
+`;
 const App = () => {
     return (
         <>
             <div>
+                <nav>
+                    <StyledLink to="/" end>MyHobby</StyledLink>
+                    <StyledLink to="/autumn" end>Autumn</StyledLink>
+                    <StyledLink to="/spring" end>Spring</StyledLink>
+                    <StyledLink to="/summer" end>Summer</StyledLink>
+                    <StyledLink to="/winter" end>Winter</StyledLink>
+                </nav>
                 
                 <Routes>
                     <Route path="/" element={<MyHobby/>} />
