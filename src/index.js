d0b071e8be9cca5ef7bf772a74da5b8ca30124e4
MateuszKar2 <system505@protonmail.com>
2024-07-29 10:34:55 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { UserContext } from 'components/userContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>   
-      <BrowserRouter>
-        <UserContext.Provider value={{username: "Władca Pierścieni"}}>
-          <App/>
-        </UserContext.Provider>
-      </BrowserRouter>
+      <UserContext.Provider value={{username: "Władca Pierścieni"}}>
+          <BrowserRouter>
+              <App/>
+          </BrowserRouter>
+      </UserContext.Provider>
   </React.StrictMode>
 );
 
