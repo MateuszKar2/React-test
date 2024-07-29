26a9154504b09fe04ccc482e1459228636d3f892
MateuszKar2 <system505@protonmail.com>
2024-07-29 14:48:09 +0200

    add note

---
+++ src/topic/10.js
@@ -0,0 +1,14 @@
+`Nawigacja`
+
+//Nawigacje można stworzyc nie tylko po kliknięciu na Link, ale również po określonym działaniu użytkownika
+//np.kliknięciu na przycisk, zdarzeniu, czy zapytaniu HTTP
+
+//Pierwszy sposób to hook useNavigate -imperatywny
+//Drugim sposobem jest hook Navigate, którym owijamy hook useNavigate -deklaratywny
+//Trzecim sposobem jest funkcja riderect  -bez interakcji użytkownika
+
+
+`łancuch zapytania`
+
+//pozwala przekazywać stan aplikacji poprzez adres URL
+//zaczyna sie od symbolu ? (1) dodaje parametry w formacie klucz-wartość (2) rozdzielone symbolem- & (3)
