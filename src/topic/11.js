2fa069a20ec3f2a85d307c1168b7f3a11eca6870
MateuszKar2 <system505@protonmail.com>
2024-07-30 21:20:47 +0200

    add file

---
+++ src/topic/11.js
@@ -0,0 +1,17 @@
+`Podstawy Redux`
+//Jedna z najpopularniejszych bibliotek do zarzadzania statusem aplikacji
+//Dzieki nie mu, mamy mamy jedno źródło prawdy, zasady co do tego jak kod powinnien być ustrukturyzowany i wygodne narzedzia przeglądarki do podgladu
+
+//Redux opiera się na architekturze Flux, który sugeruje podzielenie apki na kilka części:
+//Stores
+//Dispatcher
+//Views
+//Actions
+
+
+
+
+
+
+
+
