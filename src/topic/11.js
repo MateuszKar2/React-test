f3574ed008cb5bffae3dbfed88e53d804f84111b
MateuszKar2 <system505@protonmail.com>
2024-07-31 21:09:36 +0200

    add file

--- src/topic/11.js
+++ src/topic/11.js
@@ -8,6 +8,16 @@
 //Views
 //Actions
 
+//Sposób przekazywania propsów, powoduje że nie które komponenty wchodzą w role ,,przewodników"
+//Na początku przeżucamy metodę zmiany statusu, później sam status - Koniec z tym. We have Redux ;)
+
+//Redux rozwiązuje ten problem za pomocą store (magazynu)
+//który odpowiada za scentralizowane przechowywanie całego statusu i dostarcza zbioru zasad i metod do jego zmiany
+//Komponentom pozostaje wywołanie metody do aktualizacji danych i subskrypcja aktualizacji.
+//W ten sposób Redux rozwiązuje problemy przekazywania propsów przez całe drzewo komponentów.
+
+//Redux to tylko narzędzie, które przydaje się gdy w apce, zarzadzanie statusem jest nie do ogarniecia, w innym wypadku trudno dostrzec plusy jego użycia
+
 
 
 
