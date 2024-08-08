f33a9a2584b0d833a7697ed17798a00e6fd6b5c4
MateuszKar2 <mateuszwasabi@gmial.com>
2024-08-08 22:29:47 +0200

    add file

--- src/pages/Autumn.jsx
+++ src/pages/Autumn.jsx
@@ -1,11 +1,29 @@
-
+import { useSearchParams } from "react-router-dom";
+import { useMemo } from "react";
 
 const Autumn = () => {
+
+const [searchParams, setSearchParams] = useSearchParams();
+// const name = searchParams.get("name");
+// const color = searchParams.get("color");
+// const maxPrice = searchParams.get("maxPrice");
+
+
+const params = useMemo(
+    () => Object.fromEntries([...searchParams]),
+    [searchParams]
+);//memoizacja- przekształcenie metody get() w wygodny obiekt(przy kilku parametrach uzywanie metody get( może byc nie poręczne)
+
+
+const {name, color, maxPrice} = params;
 
     return(
         <>
             <div>
                 It's so brown.
+                <p>Name: {name}</p>
+                <p>Color: {color}</p>
+                <p>MaxPrice: {maxPrice}</p>
             </div>
         </>
     )
